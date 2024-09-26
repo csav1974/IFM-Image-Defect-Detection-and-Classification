edc26c45129a9e048c589861ed1853322c072ad7
georg <g.kuen38@gmail.com>
2024-09-26 15:45:26 +0200

    new layers and data augmentation. added Data augmentation to enhance sample size. works way better now and is easier to adapt in the future

--- modelTraining.py
+++ modelTraining.py
@@ -6,9 +6,11 @@ import random
 from tqdm import tqdm
 import tensorflow as tf
 import keras
+from sklearn.model_selection import train_test_split 
+
 
 # The path is fixed because it is only used for training the data and will not be present in the final program
-DATADIR = "dataCollection/Data/trainingData_v5"
+DATADIR = "dataCollection/Data/TrainingData_v6"
 
 CATEGORIES = [
     "Whiskers",
@@ -56,11 +58,33 @@ X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
 X = X / 255.0
 y = np.array(y)
 
+# Split data into training and validation sets
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# Data augmentation for training data
+datagen = tf.keras.preprocessing.image.ImageDataGenerator(
+    rotation_range=360,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    zoom_range=0.1,
+    horizontal_flip=True,
+    vertical_flip=True,
+    fill_mode="nearest",
+)
+
+# Data generator for validation data (no augmentation)
+validation_datagen = tf.keras.preprocessing.image.ImageDataGenerator()
+
+# Create generators
+train_generator = datagen.flow(X_train, y_train, batch_size=32)
+validation_generator = validation_datagen.flow(X_val, y_val, batch_size=32)
+
 # Create a Sequential model
 model = keras.models.Sequential()
 
 # First convolutional layer
-model.add(keras.layers.Conv2D(256, (3, 3), input_shape=(IMG_SIZE, IMG_SIZE, 1)))
+model.add(keras.layers.Input(shape=(IMG_SIZE, IMG_SIZE, 1)))
+model.add(keras.layers.Conv2D(256, (3, 3)))
 model.add(keras.layers.Activation("relu"))
 model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
 
@@ -83,21 +107,19 @@ model.add(keras.layers.Activation("softmax"))
 # Compile the model with loss='categorical_crossentropy'
 model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 
-# Data augmentation
-datagen = tf.keras.preprocessing.image.ImageDataGenerator(
-    rotation_range=360,
-    width_shift_range=0.1,
-    height_shift_range=0.1,
-    zoom_range=0.1,
-    horizontal_flip=True,
-    fill_mode="nearest",
-)
-
 # Early stopping to prevent overfitting
 early_stopping = keras.callbacks.EarlyStopping(monitor="val_loss", patience=3)
 
-# Train the model
-model.fit(X, y, batch_size=32, epochs=15, validation_split=0.3)
+# Train the model using the generators
+model.fit(
+    train_generator,
+    epochs=25,
+    validation_data=validation_generator,
+    callbacks=[early_stopping],
+    # workers=4,  # Set the number of workers for data loading
+    # use_multiprocessing=True,  # Enable multiprocessing for data loading
+    # max_queue_size=10  # Set the maximum size for the generator queue
+)
 
 # Save the model
 model_name = "fullModel_v2"
