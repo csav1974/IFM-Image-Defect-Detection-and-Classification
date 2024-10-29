36c5764504183a44c8fb66306fe1711a608585f1
georg <g.kuen38@gmail.com>
2024-10-29 15:51:55 +0100

    added conversion to real world parameters (mm)

---
+++ pixelToRealWordl.py
@@ -0,0 +1,6 @@
+
+def pixel_to_square_mm(pixel_count : int, pixel_per_square_mm : float = 316068.84):
+    return pixel_count / pixel_per_square_mm
+
+def pixel_to_mm(pixel_count : int, pixel_per_mm : float = 562.2):
+    return pixel_count / pixel_per_mm
