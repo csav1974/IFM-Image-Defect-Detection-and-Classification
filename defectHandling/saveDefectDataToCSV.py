a18e9d0d4f71bb8e9748c6272d3921d3728afb15
georg <g.kuen38@gmail.com>
2025-01-28 13:19:32 +0100

    changed layout of output File and added Positions

--- defectHandling/saveDefectDataToCSV.py
+++ defectHandling/saveDefectDataToCSV.py
@@ -1,34 +1,39 @@
 import os
 import csv
 
-
-def save_results_to_CSV(folder_path, whiskers_area, chipping_area, scratches_area, num_zeros, num_ones, ratio, whisker_count, chipping_count):
+def save_results_to_CSV(
+    folder_path,
+    whiskers_area,
+    chipping_area,
+    scratches_area,
+    num_zeros,
+    num_ones,
+    ratio,
+    whisker_count,
+    chipping_count,
+    whiskers_positions = [],
+    chipping_positions = []  
+):
     sample_name = os.path.split(folder_path)[-1]
-
 
     file_path = os.path.join(folder_path, f"{sample_name}_defectData.csv")
 
     with open(file_path, mode="w", newline="") as file:
         writer = csv.writer(file)
-        # Header
-        writer.writerow(
-            [
-                "Image_Name",
-                "Whiskers_Area_mm²",
-                "Whiskers_Count",
-                "Whiskers_Area_Relativ",
-                "Chipping_Area_mm²",
-                "Chipping_Count",
-                "Chipping_Area_Relativ",
-                "Scratches_Area_mm²",
-                "Scratches_Area_Relativ",
-                "Defect_Area_mm²",
-                "Defect_Area_Relativ"
-            ]
-        )
-        writer.writerow(
-            [sample_name, whiskers_area, whisker_count, (whiskers_area / num_ones) * 100, chipping_area, chipping_count, (chipping_area / num_ones) * 100, scratches_area, (scratches_area / num_ones) * 100, num_zeros, ratio]
-        )
 
+        writer.writerow(["Image_Name", sample_name])
+        writer.writerow(["Whiskers_Area_mm²", whiskers_area])
+        writer.writerow(["Whiskers_Count", whisker_count])
+        writer.writerow(["Whiskers_Area_Relativ", (whiskers_area / num_ones) * 100])
+        writer.writerow(["Chipping_Area_mm²", chipping_area])
+        writer.writerow(["Chipping_Count", chipping_count])
+        writer.writerow(["Chipping_Area_Relativ", (chipping_area / num_ones) * 100])
+        writer.writerow(["Scratches_Area_mm²", scratches_area])
+        writer.writerow(["Scratches_Area_Relativ", (scratches_area / num_ones) * 100])
+        writer.writerow(["Defect_Area_mm²", num_zeros])
+        writer.writerow(["Defect_Area_Relativ", ratio])
 
+        writer.writerow(["Whiskers_Position", str(whiskers_positions)])
+        writer.writerow(["Chipping_Positions", str(chipping_positions)])
+        
     print(f"CSV-Datei wurde unter {file_path} gespeichert.")
